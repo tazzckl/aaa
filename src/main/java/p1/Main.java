02bd5bbe3eb0e5b84f3bdb373dad1c4a81129531
ckl <123>
2021-11-11 15:31:40 +0800

    first commit

--- src/main/java/p1/Main.java
+++ src/main/java/p1/Main.java
@@ -2,31 +2,6 @@ package p1;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String s = "abbbcccc";
-        char max_c = s.charAt(0);
-        int maxNum = 1;
-        char c = s.charAt(0);
-        int num = 1;
-        for(int i = 2;i <= s.length();i++){
-            if(s.charAt(i - 1) == c){
-                num++;
-            }else{
-                if(num > maxNum){
-                    max_c = c;
-                    maxNum = num;
-                }
-                num = 1;
-                c = s.charAt(i - 1);
-            }
-        }
-        if(num > maxNum){
-            max_c = c;
-            maxNum = num;
-        }
-        String result = "";
-        for(int i = 1;i <= maxNum;i++){
-            result = result + max_c;
-        }
-        System.out.println(result);
+        System.out.println("hhh");
     }
 }
