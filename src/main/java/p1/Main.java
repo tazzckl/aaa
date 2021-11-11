19edf9422c5fe989ac302206bb6429a96d879ecc
ckl <123>
2021-11-11 15:47:59 +0800

    first commit

--- src/main/java/p1/Main.java
+++ src/main/java/p1/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hhh");
         System.out.println("aaa");
-        System.out.println("ccc");
+        System.out.println("ccc");//b1
     }
 }
